9d29f0cd0b2d960c75f100618e626ecb8c73e152
68Duck <josh@68duck.co.uk>
2025-01-20 13:08:09 +0000

    Fix: Fixed issue with adding bars where two scales were interacting with each other

--- addBars.R
+++ addBars.R
@@ -1,20 +1,25 @@
 library(ggplot2)
 library(cowplot)
+library(ggnewscale)
 source("./Rtest\\cloropleth.R")
 
 test <- ne_countries(scale=110, type="countries", continent="africa")
 map <- cloropleth(test, test$pop_rank, "Population rank")
 
+
+
 addBars <- function(graph, bars, x, y, measure, legend_title, scale=0.5, width=2) {
-    # print(cloropleth(bars, measure, legend_title))
-    graph <- graph + 
-        geom_rect(data=bars, aes(xmin = x - width / 2,
+    graph <- graph + ggnewscale::new_scale_fill() +
+        geom_rect(data=data.frame(bars), aes(xmin = x - width / 2,
          xmax = x + width / 2, ymin = y,
-          ymax = y + measure * scale, fill = measure, colour=measure))
-    graph$labels$colour <- legend_title
+          ymax = y + measure * scale, fill = measure)) 
+    graph <- graph + scale_fill_viridis_c(option = "A")
+    graph$labels$fill <- legend_title
     return(graph)
 }
 
+# test2 <- ne_countries(scale=110, type="countries", continent="africa")
 
-# map <- addBars(map, test, test$label_x, test$label_y, test$name_len, "Name Length")
-# print(map)
+
+map <- addBars(map, test, test$label_x, test$label_y, test$name_len, "Name Length")
+print(map)
--- cloropleth.R
+++ cloropleth.R
@@ -2,14 +2,14 @@ library(ggplot2)
 library(rnaturalearth)
 
 cloropleth <- function(data, fill, legend_title) {
-graph <- ggplot(data, aes(fill = fill)) + geom_sf() +
-    theme_void()
+graph <- ggplot() + geom_sf(data=data, aes(fill = fill)) +
+    theme_void() + scale_fill_viridis_c(option="default")
 graph$labels$fill <- legend_title
 return(graph)
 }
 
 
-# test <- ne_countries(scale=110, type="countries", continent="africa")
-# # print(cloropleth(test, test$name_len, "Name Length"))
+test <- ne_countries(scale=110, type="countries", continent="africa")
+# print(cloropleth(test, test$name_len, "Name Length"))
 # print(test)
-# print(cloropleth(test, test$pop_rank, "Population rank"))
+print(cloropleth(test, test$pop_rank, "Population rank"))
